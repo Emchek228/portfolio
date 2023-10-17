fac782ba2f1a7846f40281296f9d3d2862935fb3
Emil Imanov <emilch9@gmail.com>
2023-10-17 14:33:41 -0700

    initial commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hi, I am Emil
+          </h1>
+          <p className="text-[#adb7be] text-base sm:text-lg lg:text-xl mb-6 ">
+            This is a description
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+              Hire me
+            </button>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
+              Download CV
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] relative">
+            <Image
+              src="/avatar.png"
+              alt="avatar"
+              width={300}
+              height={300}
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
